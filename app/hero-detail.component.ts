743b59e7b60bbae2515d49e21ea4920eb8032629
Nic Flores <flores314@yahoo.com>
2017-01-01 17:21:29 -0700

    heroes angular2 app source code

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,48 @@
+import 'rxjs/add/operator/switchMap';
+import { Component, OnInit }     from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location }              from '@angular/common';
+
+import { Hero }       from './hero';
+import { HeroService } from './hero.service';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-hero-detail',
+  template:`
+    <div *ngIf="hero">
+      <h2>{{hero.name}} details!</h2>
+      <div><label>id: </label>{{hero.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="hero.name" placeholder="name"/>
+        <button (click)="save()">Save</button>
+      </div>
+    </div>
+  `
+})
+
+export class HeroDetailComponent implements OnInit {
+  hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.route.params
+        .switchMap((params: Params) => this.heroService.getHero(+params['id']))
+        .subscribe(hero => this.hero = hero);
+  }
+
+  save(): void {
+    this.heroService.update(this.hero)
+        .then(() => this.goBack());
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+}
